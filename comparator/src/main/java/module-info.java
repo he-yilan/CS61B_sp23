4b00814eddb5c69968ccfc13ff5248d6436a1045
Elana Ho <elanaho@berkeley.edu>
2023-02-04 00:21:14 -0800

    comparators

--- comparator/src/main/java/module-info.java
+++
@@ -1,9 +0,0 @@
-module com.example.comparator {
-    requires javafx.controls;
-    requires javafx.fxml;
-    requires junit;
-
-
-    opens com.example.comparator to javafx.fxml;
-    exports com.example.comparator;
-}
