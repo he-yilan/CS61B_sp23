97fe3dde098ece1c2420bfa0f46cb6377f1992ef
Elana Ho <elanaho@berkeley.edu>
2023-02-04 00:25:44 -0800

    fail

--- comparator/src/main/java/Dessert.java
+++ comparator/src/main/java/Dessert.java
@@ -8,13 +8,13 @@ import java.util.Comparator;
 public class Dessert implements Comparable<Dessert> {
     private String name;
     private int votes;
-    private int sweetness;
+    private int flavor;
     private int nutrition;
     private int cost;
 
-    public Dessert(String name, int votes, int sweetness, int nutrition, int cost) {
+    public Dessert(String name, int votes, int flavor, int nutrition, int cost) {
         this.name = name;
-        this.sweetness = sweetness;
+        this.flavor = flavor;
         this.nutrition = nutrition;
         this.cost = cost;
     }
@@ -28,31 +28,11 @@ public class Dessert implements Comparable<Dessert> {
     }
 
     public int getEnjoyment() {
-        return sweetness + nutrition - cost;
+        return flavor + nutrition - cost;
     }
 
     @Override
     public int compareTo(Dessert d) {
         return this.votes - d.getVotes(); // if positive, it means d1 is better
-    }
-
-    public class DessertComparator implements Comparator<Dessert> {
-
-        @Override
-        public int compare(Dessert d1, Dessert d2) {
-            if (d1.compareTo(d2) != 0) {
-                return d1.compareTo(d2);
-            } else {
-                int enjoyment1 = d1.getEnjoyment();
-                int enjoyment2 = d2.getEnjoyment();
-                if (enjoyment1 > enjoyment2) {
-                    return 1;
-                } else if (enjoyment2 > enjoyment1) {
-                    return -1;
-                } else {
-                    return d1.getName().compareTo(d2.getName());
-                }
-            }
-        }
     }
 }
--- comparator/src/main/java/DessertTest.java
+++ comparator/src/main/java/DessertTest.java
@@ -1,9 +1,21 @@
+import org.junit.Test;
+
 import static org.junit.Assert.*;
 
 public class DessertTest {
 
-    @org.junit.Test
-    public void compareTo() {
-        
+    @Test
+    public void compareTest() {
+        DessertComparator dc = new DessertComparator();
+        Dessert cheesecake = new Dessert("cheesecake", 4, 5, 5, 4);
+        Dessert pie = new Dessert("pie", 4, 5, 3, 5);
+        Dessert froyo = new Dessert("froyo", 2, 3, 2, 2);
+        Dessert cookie = new Dessert("cookie", 2, 2, 1, 1);
+
+        assertEquals(0, cheesecake.compareTo(pie));
+        assertEquals(1, dc.compare(cheesecake, pie));
+        assertEquals(-1, dc.compare(cookie, cheesecake));
+        assertEquals(-1, dc.compare(froyo, pie));
+        assertEquals(1, dc.compare(pie, froyo));
     }
 }
