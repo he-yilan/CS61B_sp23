582fbab4baa62e6b3f244ce374239e6d0eb4c50b
Elana Ho <elanaho@berkeley.edu>
2023-02-12 12:26:53 -0800

    design

---
+++ DebuggingWorkshop/src/DebuggingWorkshop.java
@@ -0,0 +1,31 @@
+import java.util.Date;
+
+public class DebuggingWorkshop {
+    private String description;
+    private String date;
+    private String time;
+    private String location;
+    private boolean comeToDebuggingWorkshop;
+    private int debuggingSkills;
+
+    public DebuggingWorkshop() {
+        description = "CSM Debugging Workshop!";
+        date = "February 16, 2023";
+        time = "5:00 - 6:00 PM";
+        location = "Wozniak Lounge";
+        comeToDebuggingWorkshop = true;
+        debuggingSkills = 100;
+    }
+
+    public int attend() {
+        if (comeToDebuggingWorkshop) {
+            return debuggingSkills;
+        }
+        return 0;
+    }
+
+    public static void main(String[] args) {
+        DebuggingWorkshop dw = new DebuggingWorkshop();
+        dw.attend();
+    }
+}
