efcc0d2a7bfacd68fe52b7ede16b4dea2e65cd70
Elana Ho <elanaho@berkeley.edu>
2023-02-17 19:46:35 -0800

    done

--- UnionFind/src/UnionFind.java
+++ UnionFind/src/UnionFind.java
@@ -1,39 +1,71 @@
 public class UnionFind {
-    private int[] parents;
+    private int[] nodes;
 
+    /**
+     * Creates a UnionFind data structure holding N items. Initially, all
+     * items are in disjoint sets. Each of the indices represent a node, and
+     * the values in the array are its parent. If the node does not have a parent,
+     * the value is the size of the set * -1.
+     */
     public UnionFind(int N) {
-        parents = new int[N];
+        nodes = new int[N];
 
         for (int i = 0; i < N; i++) {
-            parents[i] = -1;
+            nodes[i] = -1;
         }
     }
 
-    public boolean sameSet(int u, int v) {
+    /**
+     * Returns the size of the set node u is in.
+     */
+    public int sizeOf(int u) {
+        return -nodes[find(u)];
+    }
+
+    /**
+     * Returns whether 2 nodes are in the same set.
+     */
+    public boolean isSameSet(int u, int v) {
         return find(u) == find(v);
     }
 
+    /**
+     * Unions 2 sets together.
+     */
     public void union(int u, int v) {
-        if (sameSet(u, v)) {
+        if (isSameSet(u, v)) {
             return;
         }
         int rootU = find(u);
         int rootV = find(v);
 
-        if (rootU > rootV) { // V's set is bigger
-            parents[rootU] = rootV;
-            parents[rootV] += parents[rootU];
+        if (sizeOf(u) <= sizeOf(v)) {
+            nodes[rootV] += nodes[rootU];
+            nodes[rootU] = rootV;
         } else {
-            parents[rootV] = rootU;
-            parents[rootU] += parents[rootV];
+            nodes[rootU] += nodes[rootV];
+            nodes[rootV] = rootU;
         }
     }
 
-    public int find(int v) {
-        int root = v;
-        while (root > 0) {
-            root = parents[root];
+    /**
+     * Finds the representative (root) of the set node v is in.
+     */
+    public int find(int u) {
+        int root = u;
+        while (nodes[root] >= 0) {
+            root = nodes[root];
         }
         return root;
+    }
+
+    /**
+     * Finds the representative (root) of the set node v is in using recursion.
+     */
+    public int findRecursive(int u) {
+        if (nodes[u] < 0) {
+            return u;
+        }
+        return findRecursive(nodes[u]);
     }
 }
--- UnionFind/src/UnionFindTest.java
+++ UnionFind/src/UnionFindTest.java
@@ -8,20 +8,59 @@ public class UnionFindTest {
         UnionFind uf = new UnionFind(N);
         uf.union(1, 2);
         uf.union(2, 3);
-        assertTrue(uf.sameSet(1, 3));
+        assertTrue(uf.isSameSet(1, 3));
         for (int i = 0; i < N-1; i++) {
             uf. union(i, i+1);
         }
         for (int i = 0; i < N-1; i++) {
-            assertTrue(uf.sameSet(i, i+1));
+            assertTrue(uf.isSameSet(i, i+1));
         }
     }
 
     @org.junit.Test
     public void union() {
+        UnionFind u = new UnionFind(5);
+        u.union(0,1);
+        assertEquals(1, u.findRecursive(1));
+        assertEquals(1, u.findRecursive(0));
+        u.union(2,3);
+        u.union(0,2);
+        u.union(2, 4);
     }
 
     @org.junit.Test
     public void find() {
+        UnionFind u = new UnionFind(6);
+        u.union(0,1);
+        assertEquals(1, u.findRecursive(1));
+        assertEquals(1, u.findRecursive(0));
+        u.union(2,3);
+        u.union(0,2);
+        assertEquals(3, u.findRecursive(1));
+        assertEquals(3, u.findRecursive(2));
+        assertEquals(3, u.findRecursive(3));
+        assertEquals(4, u.sizeOf(0));
+        assertEquals(4, u.sizeOf(1));
+        assertEquals(4, u.sizeOf(2));
+        assertEquals(4, u.sizeOf(3));
+        assertEquals(1, u.sizeOf(4));
+    }
+
+    @org.junit.Test
+    public void findRecursive() {
+        UnionFind u = new UnionFind(6);
+        u.union(0,1);
+        assertEquals(1, u.findRecursive(1));
+        assertEquals(1, u.findRecursive(0));
+        u.union(2,3);
+        u.union(0,2);
+        assertEquals(3, u.findRecursive(1));
+        assertEquals(3, u.findRecursive(2));
+        assertEquals(3, u.findRecursive(3));
+        assertEquals(4, u.sizeOf(0));
+        assertEquals(4, u.sizeOf(1));
+        assertEquals(4, u.sizeOf(2));
+        assertEquals(4, u.sizeOf(3));
+        assertEquals(1, u.sizeOf(4));
     }
 }
